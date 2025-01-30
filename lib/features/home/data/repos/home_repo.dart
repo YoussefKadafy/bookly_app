dabf4a0d1c3a4ec537a41f5206e5f5c2300aa662
YoussefKadafy <kadafyyoussef@gmail.com>
2025-01-30 10:58:34 +0200

    create home repo and methods home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dart_either/dart_either.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
